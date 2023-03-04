f1be12826b1978cfcce5c0aee97d15b67f550077
Frognar <sebastian.przyszlak@gmail.com>
2023-03-04 14:31:18 +0100

    handle empty name

--- file_organizer/filename_generator.py
+++ file_organizer/filename_generator.py
@@ -9,6 +9,9 @@ def generate_new_filename(filename: str) -> str:
     date = now()
     guid = uuid.uuid4()
     file, ext = os.path.splitext(os.path.basename(filename))
+    if not file:
+        return f'{date:%Y-%m-%d}_{guid}{ext}'.lower()
+
     file = re.sub(r'\s', '-', file)
     return f'{date:%Y-%m-%d}_{file}_{guid}{ext}'.lower()
 
