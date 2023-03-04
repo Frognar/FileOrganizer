e1f5486b0b1732c6b5cc9462543eb1121b10862f
Frognar <sebastian.przyszlak@gmail.com>
2023-03-04 13:54:58 +0100

    use patch decorator

--- tests/file_lister_tests.py
+++ tests/file_lister_tests.py
@@ -5,38 +5,38 @@ from file_organizer.file_lister import list_files
 
 
 class TestFileLister(unittest.TestCase):
-    def test_list_files_returns_correct_number_of_files(self):
+    @patch('os.listdir')
+    def test_list_files_returns_correct_number_of_files(self, listdir):
+        listdir.return_value = ['file1', 'file2', 'file3']
         directory_path = "test_directory"
         expected_files_count = 3
 
-        with patch('os.listdir') as mocked_listdir:
-            mocked_listdir.return_value = ['file1', 'file2', 'file3']
-            files = list_files(directory_path)
-            self.assertEqual(len(files), expected_files_count)
+        files = list_files(directory_path)
+        self.assertEqual(len(files), expected_files_count)
 
-    def test_list_files_returns_correct_file_names(self):
-        directory_path = "test_directory"
+    @patch('os.listdir')
+    def test_list_files_returns_correct_file_names(self, listdir):
         expected_files = ["file1.txt", "file2.txt", "file3.jpg"]
+        listdir.return_value = expected_files
+        directory_path = "test_directory"
 
-        with patch('os.listdir') as mocked_listdir:
-            mocked_listdir.return_value = expected_files
-            files = list_files(directory_path)
-            self.assertEqual(files, expected_files)
+        files = list_files(directory_path)
+        self.assertEqual(files, expected_files)
 
-    def test_list_files_returns_empty_list_if_directory_is_empty(self):
-        directory_path = "empty_directory"
+    @patch('os.listdir')
+    def test_list_files_returns_empty_list_if_directory_is_empty(self, listdir):
         expected_files = []
+        listdir.return_value = expected_files
+        directory_path = "empty_directory"
 
-        with patch('os.listdir') as mocked_listdir:
-            mocked_listdir.return_value = expected_files
-            files = list_files(directory_path)
-            self.assertEqual(files, expected_files)
+        files = list_files(directory_path)
+        self.assertEqual(files, expected_files)
 
-    def test_list_files_returns_empty_list_if_directory_does_not_exist(self):
+    @patch('os.listdir')
+    def test_list_files_returns_empty_list_if_directory_does_not_exist(self, listdir):
+        listdir.side_effect = FileNotFoundError
         directory_path = "non_existing_directory"
         expected_files = []
 
-        with patch('os.listdir') as mocked_listdir:
-            mocked_listdir.side_effect = FileNotFoundError
-            files = list_files(directory_path)
-            self.assertEqual(files, expected_files)
+        files = list_files(directory_path)
+        self.assertEqual(files, expected_files)
