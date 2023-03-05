31085e86bf826e60ea29e0736ba68aeca8d9ef4e
Frognar <sebastian.przyszlak@gmail.com>
2023-03-05 02:22:08 +0100

    use os.path.join

--- file_organizer/destination_path_generator.py
+++ file_organizer/destination_path_generator.py
@@ -9,11 +9,11 @@ class DestinationPathGenerator:
     def generate_path(self, filename: str) -> str:
         _, ext = os.path.splitext(filename)
         new_path = self.get_destination_catalog(ext)
-        return f'{new_path}/{filename}'
+        return os.path.join(new_path, filename)
 
     def get_destination_catalog(self, ext: str) -> str:
         if ext in self.config:
-            return f'{self.destination_path}/{self.config[ext]}'
+            return os.path.join(self.destination_path, self.config[ext])
         elif 'default' in self.config:
-            return f'{self.destination_path}/{self.config["default"]}'
-        return f'{self.destination_path}'
+            return os.path.join(self.destination_path, self.config['default'])
+        return self.destination_path
