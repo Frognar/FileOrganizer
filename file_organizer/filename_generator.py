eb86174d5540ab1f8ecf8ae49b6939da5ea046df
Frognar <sebastian.przyszlak@gmail.com>
2023-03-05 11:55:54 +0100

    abstract generating identifier

--- file_organizer/filename_generator.py
+++ file_organizer/filename_generator.py
@@ -8,11 +8,15 @@ import uuid
 def generate_new_filename(filename: str) -> str:
     file, ext = os.path.splitext(os.path.basename(filename))
     if not file:
-        return f'{now():%Y-%m-%d}_{uuid.uuid4()}{ext}'.lower()
+        return f'{now():%Y-%m-%d}_{generate_identifier()}{ext}'.lower()
 
     file = re.sub(r'\s', '-', file)
-    return f'{now():%Y-%m-%d}_{file}_{uuid.uuid4()}{ext}'.lower()
+    return f'{now():%Y-%m-%d}_{file}_{generate_identifier()}{ext}'.lower()
 
 
 def now() -> datetime.date:
     return datetime.now()
+
+
+def generate_identifier() -> str:
+    return str(uuid.uuid4())
--- tests/filename_generator_tests.py
+++ tests/filename_generator_tests.py
@@ -7,9 +7,9 @@ from file_organizer.filename_generator import generate_new_filename
 
 class TestFilenameGenerator(unittest.TestCase):
     @patch('file_organizer.filename_generator.now')
-    @patch('uuid.uuid4')
-    def test_generate_new_filename_with_date_and_uuid(self, mock_uuid, mock_date):
-        mock_uuid.return_value = 'f50ec0b7-f960-400d-91f0-c42a6d44e3d0'
+    @patch('file_organizer.filename_generator.generate_identifier')
+    def test_generate_new_filename_with_date_and_uuid(self, mock_generate_identifier, mock_date):
+        mock_generate_identifier.return_value = 'f50ec0b7-f960-400d-91f0-c42a6d44e3d0'
         mock_date.return_value = datetime(2023, 3, 4, 14, 5)
 
         new_name = generate_new_filename('old-name.txt')
@@ -17,9 +17,9 @@ class TestFilenameGenerator(unittest.TestCase):
         self.assertEqual('2023-03-04_old-name_f50ec0b7-f960-400d-91f0-c42a6d44e3d0.txt', new_name)
 
     @patch('file_organizer.filename_generator.now')
-    @patch('uuid.uuid4')
-    def test_generate_new_filename_replacing_whitespaces_with_hyphen(self, mock_uuid, mock_date):
-        mock_uuid.return_value = 'f50ec0b7-f960-400d-91f0-c42a6d44e3d0'
+    @patch('file_organizer.filename_generator.generate_identifier')
+    def test_generate_new_filename_replacing_whitespaces_with_hyphen(self, mock_generate_identifier, mock_date):
+        mock_generate_identifier.return_value = 'f50ec0b7-f960-400d-91f0-c42a6d44e3d0'
         mock_date.return_value = datetime(2023, 3, 4, 14, 5)
 
         new_name = generate_new_filename('old name.txt')
@@ -27,9 +27,9 @@ class TestFilenameGenerator(unittest.TestCase):
         self.assertEqual('2023-03-04_old-name_f50ec0b7-f960-400d-91f0-c42a6d44e3d0.txt', new_name)
 
     @patch('file_organizer.filename_generator.now')
-    @patch('uuid.uuid4')
-    def test_generate_new_filename_using_lowercase(self, mock_uuid, mock_date):
-        mock_uuid.return_value = 'f50ec0b7-f960-400d-91f0-c42a6d44e3d0'
+    @patch('file_organizer.filename_generator.generate_identifier')
+    def test_generate_new_filename_using_lowercase(self, mock_generate_identifier, mock_date):
+        mock_generate_identifier.return_value = 'f50ec0b7-f960-400d-91f0-c42a6d44e3d0'
         mock_date.return_value = datetime(2023, 3, 4, 14, 5)
 
         new_name = generate_new_filename('OLD-NAME.txt')
@@ -37,9 +37,9 @@ class TestFilenameGenerator(unittest.TestCase):
         self.assertEqual('2023-03-04_old-name_f50ec0b7-f960-400d-91f0-c42a6d44e3d0.txt', new_name)
 
     @patch('file_organizer.filename_generator.now')
-    @patch('uuid.uuid4')
-    def test_generate_new_filename_without_extension(self, mock_uuid, mock_date):
-        mock_uuid.return_value = 'f50ec0b7-f960-400d-91f0-c42a6d44e3d0'
+    @patch('file_organizer.filename_generator.generate_identifier')
+    def test_generate_new_filename_without_extension(self, mock_generate_identifier, mock_date):
+        mock_generate_identifier.return_value = 'f50ec0b7-f960-400d-91f0-c42a6d44e3d0'
         mock_date.return_value = datetime(2023, 3, 4, 14, 5)
 
         new_name = generate_new_filename('old-name')
@@ -47,9 +47,9 @@ class TestFilenameGenerator(unittest.TestCase):
         self.assertEqual('2023-03-04_old-name_f50ec0b7-f960-400d-91f0-c42a6d44e3d0', new_name)
 
     @patch('file_organizer.filename_generator.now')
-    @patch('uuid.uuid4')
-    def test_generate_new_filename_for_empty_name(self, mock_uuid, mock_date):
-        mock_uuid.return_value = 'f50ec0b7-f960-400d-91f0-c42a6d44e3d0'
+    @patch('file_organizer.filename_generator.generate_identifier')
+    def test_generate_new_filename_for_empty_name(self, mock_generate_identifier, mock_date):
+        mock_generate_identifier.return_value = 'f50ec0b7-f960-400d-91f0-c42a6d44e3d0'
         mock_date.return_value = datetime(2023, 3, 4, 14, 5)
 
         new_name = generate_new_filename('')
