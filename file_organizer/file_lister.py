2912a443b6e172251180896bb060e40f26adad21
Frognar <sebastian.przyszlak@gmail.com>
2023-03-05 02:15:52 +0100

    ignore directories

--- file_organizer/file_lister.py
+++ file_organizer/file_lister.py
@@ -3,6 +3,6 @@ import os
 
 def list_files(directory_path: str) -> list[str]:
     try:
-        return os.listdir(directory_path)
+        return [file for file in os.listdir(directory_path) if os.path.isfile(os.path.join(directory_path, file))]
     except FileNotFoundError:
         return []
